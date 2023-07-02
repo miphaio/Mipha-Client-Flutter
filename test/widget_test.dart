7e2b6456e40793027fedf2f52fcd29a5234cbbed
wmxpy <wm@mengw.io>
2023-07-02 12:22:14 -0500

    move app package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mipha/main.dart';
+import 'package:mipha/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
