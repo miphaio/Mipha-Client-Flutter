f822af741d21f9b1f2e781ba7162eb201f89efe5
wmxpy <wm@mengw.io>
2023-07-09 19:16:07 -0500

    add chatbox for message

---
+++ lib/routes/record/widgets/chat_box.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ChatRecordChatBox extends StatefulWidget {
+  const ChatRecordChatBox({
+    super.key,
+  });
+
+  @override
+  State<ChatRecordChatBox> createState() => _ChatRecordChatBoxState();
+}
+
+class _ChatRecordChatBoxState extends State<ChatRecordChatBox> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Write message...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/routes/record/widgets/chat_record.dart
+++ lib/routes/record/widgets/chat_record.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mipha/proxy/chat/record/get/response.dart';
+import 'package:mipha/routes/record/widgets/chat_box.dart';
 import 'package:mipha/routes/record/widgets/element.dart';
 
 class ChatRecordLive extends StatefulWidget {
@@ -17,19 +18,21 @@ class ChatRecordLive extends StatefulWidget {
 class _ChatRecordLiveState extends State<ChatRecordLive> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.response.record.promptElements.length,
-      shrinkWrap: true,
-      padding: const EdgeInsets.only(
-        top: 10,
-        bottom: 10,
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: widget.response.record.promptElements.length,
+        shrinkWrap: true,
+        padding: const EdgeInsets.only(
+          top: 10,
+          bottom: 10,
+        ),
+        itemBuilder: (context, index) {
+          return ChatRecordLiveElement(
+            element: widget.response.record.promptElements[index],
+          );
+        },
       ),
-      physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) {
-        return ChatRecordLiveElement(
-          element: widget.response.record.promptElements[index],
-        );
-      },
+      bottomNavigationBar: ChatRecordChatBox(),
     );
   }
 }
