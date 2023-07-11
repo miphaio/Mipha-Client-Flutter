902cc17754962540919b8dc614b56478ca87d3c1
wmxpy <wm@mengw.io>
2023-07-10 23:07:58 -0500

    add sockat channel

--- lib/socket/socket.dart
+++ lib/socket/socket.dart
@@ -2,15 +2,26 @@ import 'package:mipha/util/log.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class MiphaSocket {
-  MiphaSocket();
-
-  WebSocketChannel? _channel;
-
-  void connect() {
-    _channel = WebSocketChannel.connect(
+  static Future<MiphaSocket> connect() async {
+    final WebSocketChannel channel = WebSocketChannel.connect(
       Uri.parse('ws://localhost:5000'),
+      protocols: [
+        'echo-protocol',
+      ],
     );
 
-    logger.debug(_channel);
+    channel.stream.listen((event) {
+      logger.info(event);
+    });
+
+    return MiphaSocket(
+      channel: channel,
+    );
   }
+
+  WebSocketChannel channel;
+
+  MiphaSocket({
+    required this.channel,
+  });
 }
