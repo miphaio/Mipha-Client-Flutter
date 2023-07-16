c95b1b9efeb3269222da314191438cf59547cb7f
wmxpy <wm@mengw.io>
2023-07-15 22:45:31 -0500

    add socket event listener

--- lib/routes/record/record.dart
+++ lib/routes/record/record.dart
@@ -1,7 +1,12 @@
+import 'package:bark/bark.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mipha/i18n/chat/localizations.dart';
+import 'package:mipha/socket/event/event.dart';
+import 'package:mipha/socket/event/event_type.dart';
 import 'package:mipha/socket/socket.dart';
+import 'package:mipha/util/authentication.dart';
+import 'package:mipha/util/log.dart';
 
 class RecordView extends StatefulWidget {
   final String chatRecordIdentifier;
@@ -16,13 +21,20 @@ class RecordView extends StatefulWidget {
 }
 
 class _RecordViewState extends State<RecordView> {
-  // final MiphaSocket _miphaSocket = MiphaSocket.createChannel();
+  MiphaSocket? _miphaSocket;
 
   bool _loading = true;
 
   @override
   void initState() {
     super.initState();
+    _establishConnection();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _miphaSocket?.channel.sink.close();
   }
 
   @override
@@ -55,5 +67,43 @@ class _RecordViewState extends State<RecordView> {
     }
 
     return Container();
+  }
+
+  Future<void> _establishConnection() async {
+    final BarkAuthenticationToken? token =
+        await barkAuthentication.ensureAuthenticationToken();
+
+    if (token == null) {
+      logger.warning("No token");
+      return;
+    }
+
+    _miphaSocket = MiphaSocket.createChannel(token);
+
+    _miphaSocket!.addEventListener(WebsocketEventType.connect, () {
+      logger.info("Connected");
+    });
+
+    _miphaSocket!.addEventListener(
+      WebsocketEventType.chatRecord,
+      (WebsocketEvent event) {
+        logger.info(event);
+      },
+    );
+
+    await _miphaSocket!.channel.ready;
+
+    _miphaSocket!.channel.sink.add({
+      "action": "chat-record",
+      "payload": {
+        "chatRecordIdentifier": "cd483131-e4d9-403c-838c-679e49685f8b",
+      }
+    });
+
+    logger.info("Connection Ready");
+
+    setState(() {
+      _loading = false;
+    });
   }
 }
--- lib/socket/socket.dart
+++ lib/socket/socket.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 
 import 'package:bark/bark.dart';
 import 'package:mipha/socket/event/event.dart';
+import 'package:mipha/socket/event/event_type.dart';
 import 'package:mipha/socket/event/parse.dart';
+import 'package:mipha/util/log.dart';
 import 'package:mipha/util/uri.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class MiphaSocket {
-  WebSocketChannel channel;
+  final WebSocketChannel channel;
+  final Map<WebsocketEventType, Set<Function>> _eventListeners = {};
 
   MiphaSocket({
     required this.channel,
@@ -31,8 +34,40 @@ class MiphaSocket {
     );
   }
 
+  void addEventListener(
+    WebsocketEventType eventType,
+    Function callback,
+  ) {
+    if (!_eventListeners.containsKey(eventType)) {
+      _eventListeners[eventType] = {};
+    }
+
+    _eventListeners[eventType]!.add(callback);
+  }
+
+  void removeEventListener(
+    WebsocketEventType eventType,
+    Function callback,
+  ) {
+    if (!_eventListeners.containsKey(eventType)) {
+      return;
+    }
+
+    _eventListeners[eventType]!.remove(callback);
+  }
+
   void _onMessage(dynamic event) {
     final dynamic json = jsonDecode(event.toString());
     final WebsocketEvent websocketEvent = parseWebsocketEvent(json);
+
+    logger.info("Message Received: $websocketEvent");
+
+    if (!_eventListeners.containsKey(websocketEvent.eventType)) {
+      return;
+    }
+
+    for (var callback in _eventListeners[websocketEvent.eventType]!) {
+      callback(websocketEvent);
+    }
   }
 }
